8f412181cc11c0fb460ce07c3bd75274aa36ec11
Johnny Reveles <Johnny Reveles@LAPTOP-PO4S021M>
2021-02-27 20:34:22 -0600

    Tipos de datos

---
+++ src/tipodedatos66659/TipoDeDatos66659.java
@@ -0,0 +1,53 @@
+/*
+ * UNIVERSIDAD AUTÓNOMA DE CAMPECHE
+ * Facultad de Ingeniería
+ * Ingeniería en Sistemas Computacionales
+ */
+package tipodedatos66659;
+
+/**
+ *
+ * @author Johnny Reveles
+ */
+public class TipoDeDatos66659 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        // VALORES MÍNIMOS Y MÁXIMOS DE CADA UNO DE LOS DATOS PRIMITIVOS
+        
+        
+        // BYTE
+        System.out.println("byteDatominimo = " + Byte.MIN_VALUE);
+        System.out.println("byteDatomaximo = " + Byte.MAX_VALUE);
+        
+        // SHORT
+        System.out.println("shortDatominimo = " + Short.MIN_VALUE);
+        System.out.println("shortDatomaximo = " + Short.MAX_VALUE);
+        
+        // INT
+        System.out.println("intDatominimo = " + Integer.MIN_VALUE);
+        System.out.println("intDatomaximo = " + Integer.MAX_VALUE);
+        
+        // LONG
+        System.out.println("longDatominimo = " + Long.MIN_VALUE);
+        System.out.println("longDatomaximo = " + Long.MAX_VALUE);
+        
+        // DOUBLE
+        System.out.println("doubleDatominimo = " + Double.MIN_VALUE);
+        System.out.println("doubleDatomaximo = " + Double.MAX_VALUE);
+        
+        
+        
+        
+        
+        
+        
+        
+        
+     
+    }
+    
+}
